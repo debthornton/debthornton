93dce78b6a6fbf2bd8ee4d17631fc49d817b0e34
Deb Thornton <debxthornton@icloud.com>
2019-05-15 23:47:24 -0400

    changes made

--- app/background.js
+++ app/background.js
@@ -49,9 +49,13 @@ header.addEventListener("mousemove", e => {
 
     // page2.style.setProperty('--x', (-e.offsetX / 100) + "px");
     // page2.style.setProperty('--y', (-e.offsetY / 50) + "px");
-
-    page.style.transform = 'translate(' + (-e.offsetX / 147) + 'px, ' + (-e.offsetY / 45) + 'px) rotateX(' + (-e.offsetX / 50000) + 'deg)';
-    page2.style.transform = 'translate(' + (-e.offsetX / 147) + 'px, ' + (-e.offsetY / 45) + 'px) rotateX(' + (-e.offsetX / 50000) + 'deg)';
+    if(window.innerWidth >= 780) {
+        page.style.transform = 'translate(' + (-e.offsetX / 147) + 'px, ' + (-e.offsetY / 45) + 'px)';
+        page2.style.transform = 'translate(' + (-e.offsetX / 147) + 'px, ' + (-e.offsetY / 45) + 'px)';
+    } else {
+        page.style.transform = '';
+        page2.style.transform = '';
+    }   
 });
 
 //control blur on background
