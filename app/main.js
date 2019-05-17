ae8dc91447fd691c1a6aa43858ab723ab5b78e36
Deb Thornton <debxthornton@icloud.com>
2019-05-17 18:45:15 -0400

    edit

--- app/main.js
+++ app/main.js
@@ -159,3 +159,7 @@ window.addEventListener('resize', () => {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
   });
+
+  window.addEventListener("orientationchange", function() {
+    document.body.style.height = window.innerHeight;
+  });
